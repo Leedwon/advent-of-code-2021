ecc8f3a9b0d5e5e58725f72167417d3fc7a16329
leedwon <leedwon12@gmail.com>
2021-12-03 22:45:12 +0100

    solve day1 challenge1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+import day1.SonarSweep
+import util.getFileIntContent
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val sonarSweep = SonarSweep()
+    val depthIncreaseSpeed = sonarSweep.calculateDepthIncreaseSpeed(getFileIntContent("/day1_challenge_1_input.txt"))
+    println(
+        buildString {
+            append("day1 task1 result = $depthIncreaseSpeed")
+        }
+    )
 }
