b2ea5f8e253f8c3a27b8451cb88526b9497a2c11
leedwon <leedwon12@gmail.com>
2021-12-07 18:24:35 +0100

    create BingoBoard.kt

---
+++ src/main/kotlin/day4/BingoBoard.kt
@@ -0,0 +1,32 @@
+package day4
+
+data class BingoBoard(
+    private val values: List<BingoElement>
+) {
+    private val sideSize = 5
+    private val expectedSize = sideSize * sideSize
+
+    val won: Boolean
+        get() = checkWinCondition()
+
+    init {
+        check(values.size == expectedSize) { "Bingo board must contain exactly $expectedSize elements" }
+    }
+
+    operator fun get(x: Int, y: Int): BingoElement = values[y * sideSize + x]
+    operator fun iterator(): Iterator<BingoElement> = values.iterator()
+
+    private fun checkWinCondition(): Boolean {
+        for (i in (0 until sideSize)) {
+            var rowCount = 0
+            var columnCount = 0
+            for (j in (0 until sideSize)) {
+                if (get(i, j).marked) rowCount++
+                if (get(j, i).marked) columnCount++
+
+            }
+            if (columnCount == sideSize || rowCount == sideSize) return true
+        }
+        return false
+    }
+}
