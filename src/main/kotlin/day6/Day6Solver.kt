60f032c68f7d4b167f33b1894bc92f8d7e9f9af4
leedwon <leedwon12@gmail.com>
2021-12-09 22:30:44 +0100

    create empty day 6 solver

---
+++ src/main/kotlin/day6/Day6Solver.kt
@@ -0,0 +1,15 @@
+package day6
+
+import util.DaySolver
+
+class Day6Solver: DaySolver {
+    override val dayDescription: String = "Day 6"
+
+    override fun solveFirstChallenge(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun solveSecondChallenge(): String {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/di/Day6Component.kt
@@ -0,0 +1,9 @@
+package di
+
+import day6.Day6Solver
+
+object Day6Component {
+
+    val day6Solver: Day6Solver
+        get() = Day6Solver()
+}
